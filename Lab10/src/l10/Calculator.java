5b3c6094b599ac7b78d9f6358160b6bb7726b35e
mac <mac@macs-mbp.home>
2025-06-22 17:46:06 +0500

    Added Calculator class with add and subtract methods

--- Lab10/src/l10/Calculator.java
+++ Lab10/src/l10/Calculator.java
@@ -2,9 +2,11 @@ package l10;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-	}
-
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
